1cdb5e247183d9df4ed0e25666768cef6218ce8f
SvetlanaPN <svetlana.peksheva@devexpress.com>
2020-06-01 18:42:10 +0300

    use Task

--- CS/Thread/Form1.cs
+++ CS/Thread/Form1.cs
@@ -21,26 +21,22 @@ using System.Text;
 using System.Windows.Forms;
 using DevExpress.XtraGrid;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace Thread
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace Thread {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
             InitData();
             FillData();
             InitGridColumns();
         }
 
         DataTable data;
-        private void InitData()
-        {
+        private void InitData() {
             data = new DataTable("ColumnsTable");
             data.BeginInit();
             AddColumn(data, "ID", System.Type.GetType("System.Int32"), true);
@@ -48,68 +44,57 @@ namespace Thread
             data.EndInit();
         }
 
-        private void AddColumn(DataTable data, string name, Type type)
-        {
+        private void AddColumn(DataTable data, string name, Type type) {
             AddColumn(data, name, type, false);
         }
-        private void AddColumn(DataTable data, string name, Type type, bool ro)
-        {
+        private void AddColumn(DataTable data, string name, Type type, bool ro) {
             DataColumn col;
             col = new DataColumn(name, type);
             col.Caption = name;
             col.ReadOnly = ro;
             data.Columns.Add(col);
         }
-        
-        private void FillData()
-        {
+
+        private void FillData() {
             FillData(data, false);
         }
 
-        private void FillData(DataTable data, bool deleteRows)
-        {
-            if (deleteRows) {
+        private void FillData(DataTable data, bool deleteRows) {
+            if(deleteRows) {
                 for(int i = data.Rows.Count - 1; i >= 0; i--)
-                    if (i % 2 != 0) 
+                    if(i % 2 != 0)
                         data.Rows.RemoveAt(i);
             }
-            else
-            {
+            else {
                 data.Clear();
-                for(int i = 0; i<= 300; i++)
-                    data.Rows.Add(new Object[] {i + 1, "Row " + (i + 1).ToString()}) ;
+                for(int i = 0; i <= 300; i++)
+                    data.Rows.Add(new Object[] { i + 1, "Row " + (i + 1).ToString() });
             }
         }
         GridControl gridControl1;
-        private void InitGridColumns()
-        {
+        private void InitGridColumns() {
             gridControl1 = new GridControl();
             gridControl1.Dock = DockStyle.Right;
             Controls.Add(gridControl1);
             gridControl1.DataSource = data;
         }
 
-        bool deleting = false;
-        delegate void UpdateDataSourceDelegate();
+        volatile bool deleting = false;
 
-        private void UpdateGridDataSource(){
-            DataTable clone = data.Copy();
-            FillData(clone, deleting);
+        private void UpdateGridDataSource(DataTable copy) {
+            FillData(copy, deleting);
             deleting = !deleting;
-            gridControl1.BeginInvoke(new MethodInvoker(delegate { gridControl1.DataSource = clone; }));
-            data = clone;
+            gridControl1.BeginInvoke(new Action(() => {
+                gridControl1.DataSource = copy;
+            }));
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e) {
             timer1.Start();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            ThreadStart threadDelegate = new ThreadStart(UpdateGridDataSource);
-            System.Threading.Thread thread = new System.Threading.Thread(threadDelegate);
-            thread.Start();
+        private void timer1_Tick(object sender, EventArgs e) {
+            Task.Run(() => UpdateGridDataSource(data.Copy()));
         }
     }
 }
